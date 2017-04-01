f336b7d500e9eda9c0490ac002440c595c83ae19
Arto Soppela <arto.soppela@gmail.com>
2017-04-01 13:59:18 +0300

    solution for module 1

---
+++ module1-solution/js/app.js
@@ -0,0 +1,49 @@
+(function () {
+'use strict';
+
+angular.module('MyModule1App', [])
+.controller('MyModule1Controller', MsgController);
+
+MsgController.$inject = ['$scope'];
+
+function MsgController($scope) {
+  $scope.lunchItemsMessage = "";
+  $scope.userLunchItemsString = "";
+  $scope.lunchItemState = "normal";
+  $scope.lunchItemState = "empty";
+
+
+  $scope.calculateLunchItems = function () {
+    console.log("calculation starts " + $scope.userLunchItemsString);
+    if ($scope.userLunchItemsString != undefined && $scope.userLunchItemsString.length > 0) {
+      var userLunchItems = $scope.userLunchItemsString.split(",");
+      userLunchItems = filterOutEmptyItems(userLunchItems);
+      $scope.lunchItemState = "normal";
+      if (userLunchItems.length < 4)
+      {
+        $scope.lunchItemsMessage = "Enjoy!";
+      }
+      else{
+        $scope.lunchItemsMessage = "Too much!";
+      }
+    }
+    else {
+      $scope.lunchItemState = "empty";
+      $scope.lunchItemsMessage = "Please enter data first";
+    }
+  };
+
+  function filterOutEmptyItems(items) {
+    var filteredItems = [];
+
+    for(var index = 0; index < items.length; index++){
+      if (items[index] != undefined && items[index].trim().length > 0) {
+        filteredItems.push(items[index]);
+      }
+    }
+
+    return filteredItems;
+  }
+}
+
+})();
